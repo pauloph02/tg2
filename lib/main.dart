f524966acc02717e5baf277aa9a5bc590c5a836c
pauloph02 <paulo_amaralcandido@hotmail.com>
2023-05-03 16:38:31 -0300

    Adicionando Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,24 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:tg/mobile/telamobile.dart';
 import 'package:tg/web/web.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 
-void main(List<String> args) {
+const firebaseOptions  = FirebaseOptions(
+  apiKey: "AIzaSyBbuHFV3FTzxBroudefmtQfkBedcdw6hvc",
+  authDomain: "trabalhograduacao-82ecd.firebaseapp.com",
+  projectId: "trabalhograduacao-82ecd",
+  storageBucket: "trabalhograduacao-82ecd.appspot.com",
+  messagingSenderId: "388399462809",
+  appId: "1:388399462809:web:9743d601bc40ef3c11fb08"
+);
+
+
+
+void main(List<String> args) async {
+  
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: firebaseOptions);
   runApp(const MyApp());
 }
 
--- lib/web/web.dart
+++ lib/web/web.dart
@@ -1,23 +1,129 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+
 class WebInicial extends StatefulWidget {
   const WebInicial({super.key});
+  
 
   @override
   State<WebInicial> createState() => _WebInicialState();
 }
 
 class _WebInicialState extends State<WebInicial> {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  FirebaseAuth auth = FirebaseAuth.instance;
   List<String> perguntas =  ["Quantos mundiais o Palmeiras já ganhou?", "Quantas libertadores o Corinthians tem?", "Quantas bolas de ouro o Neymar possui?"];
   var isWeb = kIsWeb;
-
-  
+    
   @override
   Widget build(BuildContext context) {
     perguntas.shuffle();
     return Scaffold(
-      body: Padding(
+      body: StreamBuilder<QuerySnapshot>(
+        stream: firestore.collection("questions").snapshots(),
+        builder: (context, snapshot){
+          if (!snapshot.hasData){
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          var questions = snapshot.data!.docs as List<QueryDocumentSnapshot<Map<String, dynamic>>>;
+          var pergunta = questions.map((q) => q['question']);
+          
+            return  ListView(
+              children:  [Column(
+              crossAxisAlignment: CrossAxisAlignment.center
+              ,children: [
+              
+              SizedBox(
+                
+                height: MediaQuery.of(context).size.height * .5,
+                child: Center(child: Text(pergunta.toString(), style: const TextStyle(
+                  fontSize: 30
+                ),))),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(onPressed: (){},
+                   style: ElevatedButton.styleFrom(
+                  fixedSize:  Size(MediaQuery.of(context).size.width * .4, MediaQuery.of(context).size.height *.15),
+                  backgroundColor: Colors.red
+                ), child: 
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
+                      Icon(Icons.star),
+                      SizedBox(width: 25,),
+                      Text("0")
+                    ],
+                  )
+                   ),
+                  ElevatedButton(onPressed: (){},
+                   style: ElevatedButton.styleFrom(
+                  fixedSize: Size(MediaQuery.of(context).size.width * .4, MediaQuery.of(context).size.height *.15),
+                  backgroundColor: Colors.amber
+                ),child: 
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
+                      Icon(Icons.circle),
+                      SizedBox(width: 25,),
+                      Text("1")
+                    ],
+                  ), 
+                   )
+                ],
+              ),
+              const SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(onPressed: (){},
+                   style: ElevatedButton.styleFrom(
+                  fixedSize: Size(MediaQuery.of(context).size.width * .4, MediaQuery.of(context).size.height *.15),
+                  backgroundColor: Colors.blue
+                ), child: 
+                  Row(
+                    children: const [
+                      Icon(Icons.beach_access_rounded),
+                      SizedBox(width: 25,),
+                      Text("2")
+                    ],
+                  )
+                   ),
+                  // ignore: sort_child_properties_last
+                  ElevatedButton(child: 
+                  Row(
+                    
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      const Icon(Icons.bedtime_rounded),
+                      const SizedBox(width: 25,),
+                      const Text("3")
+                    ],
+                  ),
+                   onPressed: (){},
+                   style: ElevatedButton.styleFrom(
+                  fixedSize: Size(MediaQuery.of(context).size.width * .4, MediaQuery.of(context).size.height *.15),
+                  backgroundColor: Colors.purple
+                ),
+                   )/* Text(q['question'])
+                ).toList() */
+              ])
+            ]
+            )
+            ]); 
+        },
+        
+      )
+      /* Padding(
         
         padding: const EdgeInsets.all(30),
           child: ListView(
@@ -106,7 +212,7 @@ class _WebInicialState extends State<WebInicial> {
             ]
             ),
          ]   ),
-        ),
+        ), */
         );
     
     
